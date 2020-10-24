767b9ce93a49e357b5ceabd4015f37ee892aa3f7
kanki <2050360276@qq.com>
2020-10-24 11:49:28 -0400

    first

---
+++ src/pack1/HelloWorld.java
@@ -0,0 +1,11 @@
+package pack1;
+
+/**
+ * @author : Chenqi Zhao
+ * @date :
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
